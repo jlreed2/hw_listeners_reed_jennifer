f4a2b1d23f09d50474d085d4c934031dc1a659a9
Jennifer Reed <jlreed2@ufl.edu>
2016-03-17 21:15:25 -0700

    Add click counter

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ var message = document.getElementById( 'heading' );
 
 message.addEventListener( 'click', function() {
 	var myParagraph = document.createElement( 'paragraph' );
-	myParagraph.innerHTML = '<p>This is click number XX </p';
+	var clicks = 0;
+		clicks += 1;
+	myParagraph.innerHTML = '<p>This is click number ' + clicks + ' </p>';
 	document.getElementsByClassName( 'container' )[ 0 ].appendChild( myParagraph );
 } );
